e4be8ccfc7aa673c653a734b1153e79c81bc6e84
pietro-lopes <97140255+pietro-lopes@users.noreply.github.com>
2024-10-15 21:24:53 -0300

    reworked RecipeManagerMixin to work with KubeJS

--- neoforge/src/main/java/net/mehvahdjukaar/sawmill/mixins/neoforge/RecipeManagerMixin.java
+++ neoforge/src/main/java/net/mehvahdjukaar/sawmill/mixins/neoforge/RecipeManagerMixin.java
@@ -2,11 +2,8 @@ package net.mehvahdjukaar.sawmill.mixins.neoforge;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import com.google.gson.JsonElement;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.mehvahdjukaar.sawmill.SawmillRecipeGenerator;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -14,9 +11,8 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraft.world.level.block.FireBlock;
-import net.neoforged.neoforge.common.crafting.IngredientType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -25,28 +21,22 @@ import java.util.Map;
 
 @Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
+    @Shadow
+    private Map<ResourceLocation, RecipeHolder<?>> byName;
 
+    @Shadow
+    private Multimap<RecipeType<?>, RecipeHolder<?>> byType;
 
-    @ModifyExpressionValue(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
-            at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap;builder()Lcom/google/common/collect/ImmutableMap$Builder;"))
-    public ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>> sawmill$whyCantICaptureThatLocal(ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>> original,
-                                                                                                    @Share("byName") LocalRef<ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>>> byName) {
-        byName.set(original);
-        return original;
+    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/google/common/collect/ImmutableMap$Builder;build()Lcom/google/common/collect/ImmutableMap;", shift = At.Shift.AFTER))
+    public void sawmill$addRecipes(Map<ResourceLocation, JsonElement> object, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
+        ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>> byNameCopy = ImmutableMap.builder();
+        ImmutableMultimap.Builder<RecipeType<?>, RecipeHolder<?>> byTypeCopy = ImmutableMultimap.builder();
+        this.byName.values().forEach(r -> {
+            byNameCopy.put(r.id(), r);
+            byTypeCopy.put(r.value().getType(), r);
+        });
+        SawmillRecipeGenerator.process(this.byName.values(), byNameCopy, byTypeCopy);
+        this.byName = byNameCopy.build();
+        this.byType = byTypeCopy.build();
     }
-
-    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
-            at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMultimap$Builder;build()Lcom/google/common/collect/ImmutableMultimap;",
-                    shift = At.Shift.BEFORE))
-    public void sawmill$addRecipes(Map<ResourceLocation, JsonElement> object, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci,
-                                   @Share("byName") LocalRef<ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>>> byName,
-                                   @Local ImmutableMultimap.Builder<RecipeType<?>, RecipeHolder<?>> byType) {
-        var oldRecipes = byName.get().build();
-
-        ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>> copy = ImmutableMap.builder();
-        copy.putAll(oldRecipes);
-        byName.set(copy);
-        SawmillRecipeGenerator.process(oldRecipes.values(), byName.get(), byType);
-    }
-
 }
